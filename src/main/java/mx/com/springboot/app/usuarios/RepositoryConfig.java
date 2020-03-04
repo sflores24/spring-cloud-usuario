c8641655c363f4b884533a4708ea81fa7d98cd86
sflores24 <flores.salvador@gmail.com>
2020-03-04 17:32:11 -0600

    add url name in rest service in a method for the dao

---
+++ src/main/java/mx/com/springboot/app/usuarios/RepositoryConfig.java
@@ -0,0 +1,19 @@
+package mx.com.springboot.app.usuarios;
+
+import javax.management.relation.Role;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import mx.com.springboot.app.usuarios.models.entities.Usuario;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Usuario.class, Role.class);
+	}
+
+}
--- src/main/java/mx/com/springboot/app/usuarios/models/daos/IUsuarioDAO.java
+++ src/main/java/mx/com/springboot/app/usuarios/models/daos/IUsuarioDAO.java
@@ -1,11 +1,17 @@
 package mx.com.springboot.app.usuarios.models.daos;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import mx.com.springboot.app.usuarios.models.entities.Usuario;
 
 @RepositoryRestResource(path = "usuarios")
 public interface IUsuarioDAO extends PagingAndSortingRepository<Usuario, Long> {
-	public Usuario findByUsernameAndEnabled(String username, Boolean enabled);
+	
+	//This annotation it's when we use @RepositoryRestResource
+	@RestResource(path = "findUsername")
+	public Usuario findByUsernameAndEnabled(@Param("usuario") String username, @Param("enabled") Boolean enabled);
+	//El @Param de query sirve para que se llame así el parametro en el servicio
 }
