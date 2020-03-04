456e66413d73046966e66912b1efee3c07f8652a
sflores24 <flores.salvador@gmail.com>
2020-03-04 16:52:28 -0600

    Use of @RepositoryRestResource

--- src/main/java/mx/com/springboot/app/usuarios/models/daos/IUsuarioDAO.java
+++ src/main/java/mx/com/springboot/app/usuarios/models/daos/IUsuarioDAO.java
@@ -1,9 +1,11 @@
 package mx.com.springboot.app.usuarios.models.daos;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import mx.com.springboot.app.usuarios.models.entities.Usuario;
 
+@RepositoryRestResource(path = "usuarios")
 public interface IUsuarioDAO extends PagingAndSortingRepository<Usuario, Long> {
 	public Usuario findByUsernameAndEnabled(String username, Boolean enabled);
 }
